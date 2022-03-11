765ed8e4224b492116c7ba7d7678570a24cd71a7
tysonstar912913 <tyson.star.912913@gmail.com>
2022-03-11 12:47:58 -0600

    fix conflict

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,9 +1,5 @@
 export const CONTRACT_ADDRESS = "0x76fb68F0151894F2a0e83ce54dD302e62Cf48F64";   // The contract address to Rinkeby test network
-<<<<<<< HEAD
 export const HTTP_PREFIX = "https";
-=======
-export const HTTP_PREFIX = "http";
->>>>>>> e25036162b86dcbf80a951b104be820eea8931fa
 export const API_HOST = "thegooddogclub.io";
 // export const API_PORT = "3000";
 // export const HOST = `${HTTP_PREFIX}://${API_HOST}:${API_PORT}/`;
