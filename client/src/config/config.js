73639caa6199e3422bda021ae115441fc1f9e4e3
tysonstar912913 <tyson.star.912913@gmail.com>
2022-03-11 18:43:02 +0000

    update config digitalocean

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,6 @@
 export const CONTRACT_ADDRESS = "0x76fb68F0151894F2a0e83ce54dD302e62Cf48F64";   // The contract address to Rinkeby test network
-export const HTTP_PREFIX = "http";
-export const API_HOST = "137.184.189.222";
+export const HTTP_PREFIX = "https";
+export const API_HOST = "thegooddogclub.io";
 // export const API_PORT = "3000";
 // export const HOST = `${HTTP_PREFIX}://${API_HOST}:${API_PORT}/`;
 export const HOST = `${HTTP_PREFIX}://${API_HOST}/`;
